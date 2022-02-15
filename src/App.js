3e3a18a596a555eb9635b8f220f5960e0777bbf5
ttahm3d <ttahm3d@gmail.com>
2022-02-15 23:21:08 +0530

    feat: added theme provider and switch logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { GlobalStyle, darkTheme, lightTheme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header/Header";
 
 const App = () => {
+  const [theme, setTheme] = useState("lightTheme");
+
+  const toggleTheme = () => {
+    theme === "lightTheme" ? setTheme("darkTheme") : setTheme("lightTheme");
+  };
+
   return (
     <div>
-      <h4>App</h4>
+      <ThemeProvider theme={theme === "lightTheme" ? lightTheme : darkTheme}>
+        <div className="main__container">
+          <GlobalStyle />
+          <Header toggleTheme={toggleTheme} theme={theme} />
+          <h1>App</h1>
+        </div>
+      </ThemeProvider>
     </div>
   );
 };
