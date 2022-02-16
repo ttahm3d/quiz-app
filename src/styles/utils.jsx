f0383d33e4388c68b5956dbce251173261768397
ttahm3d <ttahm3d@gmail.com>
2022-02-16 21:47:24 +0530

    feat: added primary and secondary buttons

--- src/styles/utils.jsx
+++ src/styles/utils.jsx
@@ -5,3 +5,29 @@ export const Container = styled.div`
   width: 90vw;
   margin: 0 auto;
 `;
+
+export const FlexCenter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PrimaryButton = styled.button`
+  background-color: ${(props) => props.theme.colors.blue9};
+  padding: 0.5rem;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.white};
+  margin-top: 1rem;
+  border: 1px solid ${(props) => props.theme.colors.blue7};
+  border-radius: 0.5rem; ;
+`;
+
+export const SecondaryButton = styled.button`
+  background-color: ${(props) => props.theme.colors.blue1};
+  padding: 0.5rem;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.blue11};
+  margin-top: 1rem;
+  border: 1px solid ${(props) => props.theme.colors.blue7};
+  border-radius: 0.5rem; ;
+`;
